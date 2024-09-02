b66c68bc615aac448d8af9fcef987cc54aa1b902
James Spears <jamesspearsv@gmail.com>
2024-09-02 08:01:20 -0400

    Add 404 page

--- server.js
+++ server.js
@@ -4,30 +4,41 @@ const fs = require('node:fs');
 const HOSTNAME = 'localhost';
 const PORT = 8080;
 
+function return404(err, res) {
+  const dateTime = new Date();
+  const logMessage = `\n${dateTime} -- ${err.message}\n`;
+
+  fs.appendFile('error.log', logMessage, (err) => console.error(err));
+
+  fs.readFile('./pages/404.html', 'utf-8', (err, data) => {
+    if (err) {
+      return;
+    } else {
+      res.writeHead(404, {
+        'Content-Type': 'text/html',
+      });
+
+      res.write(data);
+      return res.end();
+    }
+  });
+}
+
 const SERVER = http.createServer((req, res) => {
   const fileName = `./pages${req.url === '/' ? '/index' : req.url}.html`;
 
   fs.readFile(fileName, 'utf-8', (err, data) => {
     if (err) {
-      // console.error(err.message);
-      const dateTime = new Date();
-      const logMessage = `\n${dateTime} -- ${err.message}\n`;
-      fs.appendFile('error.log', logMessage, (err) => {
-        return;
-      });
-      res.writeHead(404, {
+      console.error(err);
+      return404(err, res);
+    } else {
+      res.writeHead(200, {
         'Content-Type': 'text/html',
       });
-      res.write('404 -- not found');
+
+      res.write(data);
       return res.end();
     }
-
-    res.writeHead(200, {
-      'Content-Type': 'text/html',
-    });
-
-    res.write(data);
-    res.end;
   });
 });
 
